1fa76e31c22a1883c4f26293a6173b476ba95df5
Franklin <43481761+Franklin-Barbosa@users.noreply.github.com>
2024-02-14 03:21:52 -0300

    Aula 02

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,33 @@
 import { CategoryButton } from "@/components/category-button"
 import { Header } from "@/components/header"
-import { View, Text, FlatList } from "react-native"
-import { CATEGORIES } from "@/utils/data/products"
-import { useState } from "react"
+import { View, Text, FlatList, SectionList } from "react-native"
+import { CATEGORIES, MENU } from "@/utils/data/products"
+import { useState, useRef } from "react"
+import { Product } from "@/components/product"
+import { Link } from "expo-router"
 
 export default function Home() {
     const [category, setCategory] = useState(CATEGORIES[0])
 
+    const selectListRef = useRef<SectionList>(null)
+
     function handleCategorySelect(selectedCategory: string) {
         setCategory(selectedCategory)
+
+        const sectionIndex = CATEGORIES.findIndex((category) => category === selectedCategory)
+
+        if (selectListRef.current) {
+            selectListRef.current.scrollToLocation({
+                animated: true,
+                sectionIndex,
+                itemIndex: 0,
+            })
+        }
     }
 
     return(
         <View className="flex-1 pt-11">
-            <Header title="Faça seu pedido" cartQuantityItems={5} />
+            <Header title="Faça seu pedido" cartQuantityItems={3} />
             
             {/* Botões */}
             <FlatList 
@@ -30,6 +44,26 @@ export default function Home() {
                 className="max-h-10 mt-5" // espaçamento superior dos botões
                 showsHorizontalScrollIndicator={false} // remover barra so rolar botões
                 contentContainerStyle={{gap: 12, paddingHorizontal: 20}} // espaçamento entre os botões e ao lado esquerdo da tela
+            />
+
+            <SectionList 
+                ref={selectListRef}
+                sections={MENU}
+                keyExtractor={(item) => item.id}
+                stickySectionHeadersEnabled={false} // evitar esticamento ou sess~es sobrepostas
+                renderItem={({ item }) => ( 
+                    <Link href={`/product/${item.id}`} asChild >
+                        <Product data={item} />
+                    </Link> 
+                )} // renderizar itens da lista
+                renderSectionHeader={({section: {title}}) => (
+                    <Text className="text-xl text-white font-heading mt-8 mb-2">
+                        {title}
+                    </Text>
+                )} // renderizar sessões
+                className="flex-1 p-5"
+                showsVerticalScrollIndicator={false} // desabilitando barra de rolagen vertical
+                contentContainerStyle={{ paddingBottom: 100 }} // espaçamento no fim da lista
             />
         </View>
     )
